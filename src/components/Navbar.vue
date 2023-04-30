ad9b7747ebb8975580462e56ea04570cf03221b0
ArneSamson <arnesamson007@gmail.com>
2023-04-30 16:38:35 +0200

    feat: added navbar.vue with chatGPT code. Also deleted the starter template icons and helloworld.vue.

---
+++ src/components/Navbar.vue
@@ -0,0 +1,108 @@
+<template>
+    <nav class="navbar">
+        <!-- Left side of navbar -->
+        <div class="navbar-left">
+        <router-link to="/" class="navbar-logo">
+            <img src="/path/to/logo.png" alt="Logo">
+        </router-link>
+        <router-link to="/dashboard" class="navbar-link">Dashboard</router-link>
+        <router-link to="/identification" class="navbar-link">Identification</router-link>
+        <router-link to="/activities" class="navbar-link">Activities</router-link>
+        </div>
+
+        <!-- Right side of navbar -->
+        <div class="navbar-right">
+        <button class="navbar-btn" @click="showPopup">Popup</button>
+        <button class="navbar-btn" @click="showNotifications">Notifications</button>
+        <router-link to="/settings" class="navbar-link">Settings</router-link>
+        <div class="navbar-user">
+            <span class="navbar-user-greeting">Welcome, {{ username }}</span>
+            <span class="navbar-user-function">{{ userFunction }}</span>
+        </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+    data() {
+        return {
+        username: "John", // Replace with actual username
+        userFunction: "Team Leader" // Replace with actual user function
+        };
+    },
+    methods: {
+        showPopup() {
+        // Add code to show popup
+        },
+        showNotifications() {
+        // Add code to show notifications
+        }
+    }
+    };
+</script>
+  
+<style>
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  background-color: #333;
+  color: #fff;
+}
+
+.navbar-left {
+  display: flex;
+  align-items: center;
+}
+
+.navbar-logo {
+  margin-right: 2rem;
+}
+
+.navbar-link {
+  margin-right: 2rem;
+  color: #fff;
+  text-decoration: none;
+}
+
+.navbar-link:hover {
+  text-decoration: underline;
+}
+
+.navbar-right {
+  display: flex;
+  align-items: center;
+}
+
+.navbar-btn {
+  margin-right: 2rem;
+  padding: 0.5rem 1rem;
+  border: none;
+  border-radius: 0.25rem;
+  background-color: #fff;
+  color: #333;
+  cursor: pointer;
+}
+
+.navbar-btn:hover {
+  background-color: #eee;
+}
+
+.navbar-user {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+}
+
+.navbar-user-greeting {
+  font-size: 1.25rem;
+}
+
+.navbar-user-function {
+  font-size: 0.75rem;
+  color: #ccc;
+}
+
+</style>
