a2025a78aa632a2603d1ea3029c7be14b23656da
ArneSamson <arnesamson007@gmail.com>
2023-05-06 13:24:19 +0200

    feat: added sideshoots menu for identification tab

--- src/views/Identification.vue
+++ src/views/Identification.vue
@@ -7,18 +7,33 @@
 
     <div class="pop-ups">
       <IllnessesFolddown/>
+      <SideshootsFolddown/>
     </div>
 
   </template>
   
   <script>
   import IllnessesFolddown from '../components/Illnesses-folddown.vue';
+  import SideshootsFolddown from '../components/Sideshoots-folddown.vue';
 
   export default {
     name: 'Identification',
     components: {
-    IllnessesFolddown
+    IllnessesFolddown,
+    SideshootsFolddown
     }
   }
   </script>
   
+<style>
+.top-padding{
+  padding-top: 40px;
+}
+
+.pop-ups{
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding-top: 40px;
+}
+</style>
