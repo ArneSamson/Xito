50a77927a691ac52b04aa48a119e3c24f487c622
ArneSamson <arnesamson007@gmail.com>
2023-05-10 11:34:35 +0200

    feat: added fixed nav to Live tab

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Dashboard from './views/Dashboard.vue'
 import Identification from './views/Identification.vue'
 import Activities from './views/Activities.vue'
+import Live from './views/Live.vue'
 import Profile from './views/Profile.vue'
 import Settings from './views/Settings.vue'
 import TimelineDetails from './views/Timeline-Details.vue'
@@ -12,6 +13,7 @@ const routes = [
   { path: '/dashboard', component: Dashboard },
   { path: '/identification', component: Identification },
   { path: '/activities', component: Activities },
+  {path: '/live', component: Live},
   { path: '/profile', component: Profile },
   { path: '/settings', component: Settings },
   { path: '/timeline-details/:day', name: 'Timeline-details', component: TimelineDetails },
--- src/views/Live.vue
+++ src/views/Live.vue
@@ -1,23 +1,22 @@
 <template>
     <div>
-      <h1>Activities</h1>
-      <!-- add your settings content here -->
-      <div class="All">
-        <Tasks/>
-      </div>
-      <div class="My">
+        <h1>Live</h1>
+        <!-- add your settings content here -->
+        <div class="All">
 
-      </div>
+        </div>
+        <div class="My">
+
+        </div>
     </div>
-  </template>
-  
-  <script>
-  import Tasks from '../components/Tasks.vue';
+</template>
 
-  export default {
-    name: 'Activities',
-    components: {
-      Tasks
-    }
-  }
-  </script>
+<script>
+
+export default {
+name: 'Activities',
+components: {
+    // Tasks
+}
+}
+</script>
