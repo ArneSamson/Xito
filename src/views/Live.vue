47beb03a5cc01a517bb6a8282b5324e87ad2b900
ArneSamson <arnesamson007@gmail.com>
2023-05-10 11:27:35 +0200

    feat: added Live tab

---
+++ src/views/Live.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <h1>Activities</h1>
+      <!-- add your settings content here -->
+      <div class="All">
+        <Tasks/>
+      </div>
+      <div class="My">
+
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import Tasks from '../components/Tasks.vue';
+
+  export default {
+    name: 'Activities',
+    components: {
+      Tasks
+    }
+  }
+  </script>
