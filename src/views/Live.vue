ce0309e894a6451887ad6481190d894830bab117
ArneSamson <arnesamson007@gmail.com>
2023-05-19 16:13:52 +0200

    feat: working image recognition from camera feed when pressing button

--- src/views/Live.vue
+++ src/views/Live.vue
@@ -2,7 +2,7 @@
   <div>
     <div>
       <!-- add code for displaying the video here -->
-      <video ref="video" width="300" height="200" autoplay></video>
+      <video ref="video" width="300" height="200" controls autoplay></video>
     </div>
 
     <div>
@@ -18,68 +18,62 @@
 </template>
 
 <script>
-import * as ml5 from "ml5";
+
+import * as ml5 from 'ml5';
 
 export default {
-  data() {
-    return {
-      message: "Please wait...loading model...",
-      video: null,
-      classifier: null,
-      isModelLoaded: false,
-      predictions: null,
-      identityImage: null,
-      canvas: null,
-      ctx: null,
-      imageData: null,
-    };
-  },
-
   mounted() {
-    this.video = this.$refs.video;
+  
+  // add code for loading the model and making predictions here
+  const message = document.querySelector("#message");
+  // Controls the message showing on the screen
+  const checkButton = document.querySelector("#button");
+  // Contains the uploaded image
+
+  const video = this.$refs.video;
     navigator.mediaDevices
       .getUserMedia({ video: true })
       .then((stream) => {
-        this.video.srcObject = stream;
-        this.video.play();
+        video.srcObject = stream;
+        video.play();
       })
       .catch((error) => {
         console.error("Error accessing webcam:", error);
       });
 
-    this.classifier = ml5.imageClassifier("../../public/model/model.json", this.onModelLoaded);
-  },
+  checkButton.addEventListener("click", () => userImageUploaded());
 
-  methods: {
-    onModelLoaded() {
-      this.message = "Model loaded!";
-      this.isModelLoaded = true;
-      console.log("Model loaded!");
-      console.log(this.classifier);
-    },
-    checkFrame() {
-      this.canvas = document.getElementById("myCanvas"); // Add this line
-      this.ctx = this.canvas.getContext('2d'); // Update this line
+  // Initialize the Image Classifier method with Xito model
+  const classifier = ml5.imageClassifier('../../public/model/model.json', modelLoaded);
 
-      this.ctx.drawImage(this.video, 0, 0, this.canvas.width, this.canvas.height); // Update this line
-
-      this.imageData = this.canvas.toDataURL("image/jpeg", 0.75);
+  function userImageUploaded() {
+      const canvas = document.getElementById("myCanvas");
+      const ctx = canvas.getContext("2d");
+      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+      const imageData = canvas.toDataURL("image/jpeg", 0.75);
 
       const image = new Image();
-      image.src = this.imageData;
+      image.src = imageData;
+      message.innerHTML = "Image was loaded!"
 
+      classifier.classify(image, (err, results) => {
+          console.log(results);
+          message.innerHTML = `
+          ${results[0].label} : ${results[0].confidence * 100}% 
+          <br> ${results[1].label} : ${results[1].confidence * 100}%
+          <br> ${results[2].label} : ${results[2].confidence * 100}%
+          <br> ${results[3].label} : ${results[3].confidence * 100}%
+          <br> ${results[4].label} : ${results[4].confidence * 100}%
+          `
+      });
+  }
 
-      this.classifier.classify(this.imageData, (err, results) => {
-            console.log(results);
-            this.message.innerHTML = `
-            ${results[0].label} : ${results[0].confidence * 100}% 
-            <br> ${results[1].label} : ${results[1].confidence * 100}%
-            <br> ${results[2].label} : ${results[2].confidence * 100}%
-            <br> ${results[3].label} : ${results[3].confidence * 100}%
-            <br> ${results[4].label} : ${results[4].confidence * 100}%
-            `
-        });
-    },
-  },
-};
+  // When the model is loaded
+  function modelLoaded() {
+      message.innerHTML = "Model Loaded"
+      console.log('Model Loaded!');
+  }
+
+      }
+  }
 </script>
