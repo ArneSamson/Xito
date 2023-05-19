0443a97e0115aba2db44a3c6f90e706cffd510f0
ArneSamson <arnesamson007@gmail.com>
2023-05-19 14:12:04 +0200

    feat: working snapshot system

--- src/views/Live.vue
+++ src/views/Live.vue
@@ -1,74 +1,80 @@
 <template>
   <div>
-    <!-- add code for displaying the video here -->
     <div>
-      <video id="video" width="300" height="200" autoplay></video>
+      <!-- add code for displaying the video here -->
+      <video ref="video" width="300" height="200" autoplay></video>
     </div>
 
-    <!-- button that checks the current frame of the video -->
     <div>
-      <button type="button" id="button">Check Frame</button>
+      <!-- button that checks the current frame of the video -->
+      <button type="button" id="button" @click="checkFrame">Check Frame</button>
     </div>
 
-    <!-- display the result of the classification -->
-    <div id="message">Please wait...loading model...</div>
+    <canvas id="myCanvas" width="300" height="200"></canvas>
+
+  
+    <div id="message">{{ message }}</div>
   </div>
 </template>
 
 <script>
-import * as ml5 from 'ml5';
+import * as ml5 from "ml5";
 
 export default {
+  data() {
+    return {
+      message: "Please wait...loading model...",
+      video: null,
+      classifier: null,
+      isModelLoaded: false,
+      predictions: null,
+      identityImage: null,
+      canvas: null,
+      ctx: null,
+    };
+  },
+
   mounted() {
-    // add code for loading the model and making predictions here
-    const message = document.querySelector("#message");
-    // Controls the message showing on the screen
-    const checkButton = document.querySelector("#button");
-    // Button to check the current frame of the video
-    const video = document.querySelector("#video");
-    // Contains the webcam feed
-
-    // Initialize the Image Classifier method with Xito model
-    const classifier = ml5.imageClassifier('../../public/model/model.json', modelLoaded);
-
-    function modelLoaded() {
-      message.innerHTML = "Please allow access to your webcam!"
-      console.log('Model Loaded!');
-    }
-
-    checkButton.addEventListener("click", event => {
-      classifier.classify((err, results) => {
-        console.log(results);
-        message.innerHTML = `
-          ${results[0].label} : ${results[0].confidence * 100}% 
-          <br> ${results[1].label} : ${results[1].confidence * 100}%
-          <br> ${results[2].label} : ${results[2].confidence * 100}%
-          <br> ${results[3].label} : ${results[3].confidence * 100}%
-          <br> ${results[4].label} : ${results[4].confidence * 100}%
-        `;
-
-        if (results[0].confidence > 0.7) {
-          // save the current frame of the video
-          const canvas = document.createElement('canvas');
-          canvas.width = video.videoWidth;
-          canvas.height = video.videoHeight;
-          canvas.getContext('2d').drawImage(video, 0, 0);
-          const dataURL = canvas.toDataURL();
-          console.log(dataURL);
-        }
-      });
-    });
-
-    // get access to the webcam
-    navigator.mediaDevices.getUserMedia({ video: true })
-      .then(stream => {
-        video.srcObject = stream;
-        message.innerHTML = "Please click the button to check the current frame of the video.";
+    this.video = this.$refs.video;
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then((stream) => {
+        this.video.srcObject = stream;
+        this.video.play();
       })
-      .catch(err => {
-        message.innerHTML = "Could not access webcam!";
-        console.error(err);
+      .catch((error) => {
+        console.error("Error accessing webcam:", error);
       });
-  }
+
+    this.classifier = ml5.imageClassifier(
+      "../../public/model/model.json",
+      this.onModelLoaded
+    );
+  },
+
+  methods: {
+    onModelLoaded() {
+      this.message = "Model loaded!";
+      this.isModelLoaded = true;
+      console.log("Model loaded!");
+    },
+    checkFrame() {
+      this.canvas = document.getElementById("myCanvas"); // Add this line
+      this.ctx = this.canvas.getContext('2d'); // Update this line
+
+      this.ctx.drawImage(this.video, 0, 0, this.canvas.width, this.canvas.height); // Update this line
+
+      this.classifier.classify(this.canvas, (err, results) => {
+            console.log(results);
+            message.innerHTML = `
+            ${results[0].label} : ${results[0].confidence * 100}% 
+            <br> ${results[1].label} : ${results[1].confidence * 100}%
+            <br> ${results[2].label} : ${results[2].confidence * 100}%
+            <br> ${results[3].label} : ${results[3].confidence * 100}%
+            <br> ${results[4].label} : ${results[4].confidence * 100}%
+            `
+        });
+    },
+  },
 };
 </script>
