63e76fa9028995b35b67ac46232cc87bb10ad559
WillTDP <williamhazevoets@gmail.com>
2023-06-12 17:58:32 +0200

    FEAT: basic styling for tasks and activities with small html changes so that each outputted element can be styled

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,35 +1,38 @@
 <template>
     <div class="Tasks">
-        <div>
-            <div>
-                <h2>All Tasks</h2>
-                <div v-for="(team, index) in All" :key="index" class="divider">
-                    <h2>{{ team.team }}</h2>
-                    <div v-for="(task, index) in team.tasks" :key="index">
-                        <h3>{{ task.title }}</h3>
-                        <p>{{ task.message }}</p>
-                        <p>Row {{ task.row }}, Block {{ task.block }}</p>
-                        <p>{{ task.date }}, {{ task.time }}</p>
-                        <p>{{ task.person }}</p>
-                        <div class="status-circle" :style="{ 'background-color': getStatusColor(task.status) }"></div>
-                    </div>
-                </div>
+      <div class="All">
+        <h2>All Tasks</h2>
+        <div v-for="(team, index) in All" :key="index" class="divider">
+          <h2>{{ team.team }}</h2>
+          <div v-for="(task, index) in team.tasks" :key="index" class="task">
+            <h3>{{ task.title }}</h3>
+            <p>{{ task.message }}</p>
+            <p>Row {{ task.row }}, Block {{ task.block }}</p>
+            <p>{{ task.date }}, {{ task.time }}</p>
+            <p>{{ task.person }}</p>
+            <div class="status-circle">
+              <div class="status-dot" :style="{ backgroundColor: getStatusColor(task.status) }"></div>
             </div>
-
-            <div>
-                <h2>My Tasks</h2>
-                <div v-for="(task, index) in My.tasks" :key="index">
-                    <h3>{{ task.title }}</h3>
-                    <p>{{ task.message }}</p>
-                    <p>Row {{ task.row }}, Block {{ task.block }}</p>
-                    <p>{{ task.date }}, {{ task.time }}</p>
-                    <p>{{ task.person }}</p>
-                    <div class="status-circle" :style="{ 'background-color': getStatusColor(task.status) }"></div>
-                </div>
-            </div>
+          </div>
         </div>
+      </div>
+  
+      <div class="My">
+        <h2>My Tasks</h2>
+        <div v-for="(task, index) in My.tasks" :key="index" class="task">
+          <h3>{{ task.title }}</h3>
+          <p>{{ task.message }}</p>
+          <p>Row {{ task.row }}, Block {{ task.block }}</p>
+          <p>{{ task.date }}, {{ task.time }}</p>
+          <p>{{ task.person }}</p>
+          <div class="status-circle">
+            <div class="status-dot" :style="{ backgroundColor: getStatusColor(task.status) }"></div>
+          </div>
+        </div>
+      </div>
     </div>
   </template>
+  
   
   <script>
   export default {
@@ -127,18 +130,78 @@
   </script>
   
   <style scoped>
+    .Tasks {
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        align-items: flex-start;
+    }
+    .All {
+        display: flex;
+        flex-direction: column;
+        margin-right: 5rem;
+    }
+    .My {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .Tasks h2{
+        font-family: "Proxima Soft SemiBold";
+        font-size: 2rem;
+        margin-bottom: 1rem;
+    }
+    .Tasks h3{
+        font-family: "Proxima Soft SemiBold";
+        font-size: 1.5rem;
+        margin-bottom: 0.5rem;
+    }
+
+    .Tasks p{
+        font-family: "Poppins Light";
+        font-size: 1rem;
+        margin-bottom: 0.5rem;
+    }
+
+
   ul{
     list-style-type: none;
   }
   /* .divider{
 
-  } */
-  .status-circle {
+  } */.status-circle {
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  margin-right: 5px;
+}
+
+.status-dot {
   display: inline-block;
   width: 10px;
   height: 10px;
   border-radius: 50%;
-  margin-right: 5px;
 }
+
+/* Define your status colors */
+.status-dot.red {
+  background-color: red;
+}
+
+.status-dot.yellow {
+  background-color: yellow;
+}
+
+.status-dot.green {
+  background-color: green;
+}
+
+.status-dot.gray {
+  background-color: gray;
+}
+
+
+
   </style>
   
